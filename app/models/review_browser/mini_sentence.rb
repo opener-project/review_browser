4e7c630662c2ceebff47765b3882aa7d12888055
Giannis Melidis <giannismelidis@olery.com>
2013-08-28 17:29:27 +0200

    added related methods (not tested)

--- app/models/review_browser/mini_sentence.rb
+++ app/models/review_browser/mini_sentence.rb
@@ -3,5 +3,20 @@ module ReviewBrowser
     attr_accessible :body, :review_id, :sentiment, :topic
     
     belongs_to :review
+    
+    def self.same_topic
+      where(:topic => topic)
+    end
+    
+    def self.related
+      same_topic.where(create_query(body))
+        )
+    end
+    
+    def create_query(body)
+      body.split(" ").map do |word|
+        "(body LIKE '%#{word}%')"
+      end.uniq.join(" OR ")
+    end
   end
 end
