cb3ba1fedc1b191711527e25b8646d0858ebf079
Giannis Melidis <giannismelidis@olery.com>
2013-08-29 11:25:41 +0200

    removed review rating model, it's not needed. we have rating model now.

--- app/models/review_browser/review_rating.rb
+++
@@ -1,9 +0,0 @@
-module ReviewBrowser
-  class ReviewRating < ActiveRecord::Base
-    attr_accessible :review_id, :topic, :value
-    
-    belongs_to :review
-    
-    validates_uniqueness_of :topic, :scope => [:review_id]
-  end
-end
