c32c3c92eab2b27a5ebd17230104883ece458f08
Giannis Melidis <giannismelidis@olery.com>
2013-08-30 10:09:56 +0200

    removed binding.pry in review importer

--- lib/review_browser/review_importer.rb
+++ lib/review_browser/review_importer.rb
@@ -17,7 +17,6 @@ module ReviewBrowser
     end
 
     def parse_review(row)
-      binding.pry
       review = create_review(row[0], row[1], row[11], row[10])
       create_reviewer(review, row[8], row[9])
       create_ratings(review, row[2], row[3], row[4], row[5], row[6], row[7])
