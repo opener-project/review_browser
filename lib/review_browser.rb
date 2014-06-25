ce26cbef683908c4bcb3db0eb9c6a70603cdd2ea
Yorick Peterse <yorickpeterse@olery.com>
2014-06-25 17:49:56 +0200

    Removed useless comments.

--- lib/review_browser.rb
+++ lib/review_browser.rb
@@ -1,18 +1,7 @@
-# If you want to use the ActiveModel serializers Gem, uncomment the following
-# line:
-#require 'active_model_serializers'
-
 require 'haml-rails'
 require 'jquery-rails'
 require 'kaminari'
 require 'opener/client'
-
-# If your engine needs the Olery models Gem (it probably will), uncomment the
-# following lines and add any extra ones where needed:
-#
-#require 'olery/models'
-#require 'olery/models/active_record/following'
-#require 'olery/models/mongoid/review'
 
 require_relative 'review_browser/engine'
 require_relative 'review_browser/related_opinions_finder'
